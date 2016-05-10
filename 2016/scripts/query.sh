a0f05d6bd1a930588932da9b848e550255acfd45
Sachin Patil <psachin@redhat.com>
2016-05-10 20:01:21 +0530

    Get auth token via curl

--- 2016/scripts/query.sh
+++ 2016/scripts/query.sh
@@ -1,6 +1,12 @@
 publicURL="http://192.168.8.80:8080/v1/AUTH_test"
 token="AUTH_tkd749c417e2c049af898f0960f5230958"
 
+# Get token
+curl -v \
+     -H "X-Auth-User: test:tester" \
+     -H "X-Auth-Key: testing" \
+     http://192.168.8.80:8080/auth/v1.0/
+
 #curl -i $publicURL/archive -X PUT -H "Content-length: 0" -H "X-Auth-Token: $token"
 
 # Create a 'current' container to store current version of an object
@@ -55,4 +61,6 @@ token="AUTH_tkd749c417e2c049af898f0960f5230958"
 
 # curl -X GET -i -H "X-Auth-Token: $token" $publicURL/archive?delimiter=/
 
-curl -i $publicURL/p-archive?prefix=008cat6.jpg -X GET -H "X-Auth-Token: $token"
+# curl -i $publicURL/p-archive?prefix=008cat6.jpg -X GET -H "X-Auth-Token: $token"
+
+
